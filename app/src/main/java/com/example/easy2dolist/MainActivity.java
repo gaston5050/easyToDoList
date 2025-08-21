0835796567d20680e488e2f987a4c942c0d945d9
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2025-08-21 16:58:27 -0300

    prueba de clase task 1

---
+++ app/src/main/java/com/example/easy2dolist/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.easy2dolist;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+
+import com.example.easy2dolist.model.Status;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.easy2dolist.databinding.ActivityMainBinding;
+
+import com.example.easy2dolist.model.Task;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Task dummies
+    Task task1 = new Task(1, "Task 1", "Description 1", Status.PENDING);
+    Task task2 = new Task(2, "Task 2", "Description 2", Status.COMPLETED);
+    log.d("TEST_TASK", "Task 1: " + task1.getTitle()) + task1.getDescription() + task1.getStatus();
+
+
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null)
+                        .setAnchorView(R.id.fab).show();
+            }
+        });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
---
+++ app/src/main/java/com/example/easy2dolist/model/Status.java
@@ -0,0 +1,8 @@
+package com.example.easy2dolist.model;
+
+public enum Status {
+    PENDING,
+    COMPLETED,
+    CANCELED,
+    IN_PROGRESS
+}
