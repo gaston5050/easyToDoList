8753a1360625b5d706cf53e42547bb943bc6a290
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2025-08-22 16:37:43 -0300

    trabajando en el adapter

---
+++ app/src/main/java/com/example/easy2dolist/ui/task/TaskAdapter.java
@@ -0,0 +1,7 @@
+package com.example.easy2dolist.ui.task;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public abstract class TaskAdapter  extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
+
+}
