c6d8df8e632cd3521cab681bbb5ce9380d0f4408
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2025-08-26 16:51:20 -0300

    Si

--- app/src/main/java/com/example/easy2dolist/ui/task/TaskAdapter.java
+++ app/src/main/java/com/example/easy2dolist/ui/task/TaskAdapter.java
@@ -1,7 +1,14 @@
 package com.example.easy2dolist.ui.task;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.easy2dolist.R;
 import com.example.easy2dolist.model.Task;
 
 import java.util.ArrayList;
@@ -9,19 +16,44 @@ import java.util.List;
 
 public class TaskAdapter  extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
 
-    public interface Listener {
-        void onToggle(Task task);
-        void onClick(Task task);
-        void onLongPress(Task task);
+ private List<Task> tasks = new ArrayList<>();
+
+ public void submitList(List<Task> newTask) {
+     tasks = new ArrayList<>(newTask);
+     notifyDataSetChanged();
+ }
+
+
+    @NonNull
+    @Override
+    public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.item_task, parent, false);
+        return new TaskViewHolder(v);
     }
-    private final Listener listener;
-    private final List<Task> items = new ArrayList<>();
 
-    public TaskAdapter(Listener listener) {
-        this.listener = listener;
+    @Override
+    public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
+     Task t = tasks.get(position);
+     holder.txtTitulo.setText(t.getTitle());
+     holder.txtDescripcion.setText(t.getDescription());
+     holder.txtEstado.setText(t.getStatus().toString());
+
     }
 
+    @Override
+    public int getItemCount() {
+        return tasks.size();
+    }
 
+    static class TaskViewHolder extends RecyclerView.ViewHolder {
+      TextView txtTitulo, txtDescripcion,  txtEstado;
 
-
+        TaskViewHolder(@NonNull View itemView) {
+         super(itemView);
+        txtTitulo = itemView.findViewById(R.id.txtTitulo);
+        txtDescripcion = itemView.findViewById(R.id.txtDescripcion);
+        txtEstado = itemView.findViewById(R.id.txtEstado);
+        }
+    }
 }
